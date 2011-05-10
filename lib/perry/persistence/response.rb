c7ec8bf075bf820fe98457eb6cbccf395769908b
Jordan Bach <jordanbach@gmail.com>
2011-05-10 12:29:19 -0500

    added an initializer for the Response class

--- lib/perry/persistence/response.rb
+++ lib/perry/persistence/response.rb
@@ -1,5 +1,12 @@
 module Perry::Persistence
   class Response
-    attr_accessor :success, :status, :meta, :raw, :format, :model_attributes, :errors
+    ATTRIBUTES = [:success, :status, :meta, :raw, :format, :model_attributes, :errors]
+    attr_accessor *ATTRIBUTES
+
+    def initialize(attrs={})
+      ATTRIBUTES.each do |attr|
+        self.send("#{attr}=", attrs[attr])
+      end
+    end
   end
 end
--- test/unit/response_test.rb
+++ test/unit/response_test.rb
@@ -1,18 +1,29 @@
 require "#{File.dirname(__FILE__)}/../test_helper"
 
 class Perry::ResponseTest < Test::Unit::TestCase
+
+  RESPONSE_ATTRIBUTES = Perry::Persistence::Response::ATTRIBUTES
+
   context "Perry::Persistance::Response" do
     setup do
       @response = Perry::Persistence::Response.new
     end
 
-    RESPONSE_ATTRIBUTES = [:success, :status, :meta, :raw, :format, :model_attributes, :errors]
     RESPONSE_ATTRIBUTES.each do |attr|
       should "define a reader for the #{attr} attribute" do
         assert @response.respond_to?(attr)
       end
       should "define a writer for the #{attr} attribute" do
         assert @response.respond_to?("#{attr}=")
+      end
+    end
+
+    should "assign attributes in initializer" do
+      attrs = RESPONSE_ATTRIBUTES.inject({}) { |hash, attr| hash.merge(attr => :foo) }
+      response = @response.class.new(attrs)
+
+      attrs.each do |attr_name, attr_value|
+        assert_equal attr_value, response.send(attr_name)
       end
     end
   end
